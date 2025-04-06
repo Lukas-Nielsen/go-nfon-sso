623b0c379af45e4126fea654b5effbf0f6502aa1
Lukas Nielsen <moin@lukasnielsen.de>
2025-04-06 16:58:31 +0200

    v0.0.4

---
+++ converter.go
@@ -0,0 +1,27 @@
+package nfon
+
+type Data struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type Link struct {
+	Rel  string `json:"link"`
+	Href string `json:"href"`
+}
+
+func DataToMap(data []Data) map[string]any {
+	result := make(map[string]any)
+	for _, entry := range data {
+		result[entry.Name] = entry.Value
+	}
+	return result
+}
+
+func LinksToMap(data []Link) map[string]string {
+	result := make(map[string]string)
+	for _, entry := range data {
+		result[entry.Rel] = entry.Href
+	}
+	return result
+}
